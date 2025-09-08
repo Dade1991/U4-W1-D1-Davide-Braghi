3a3de8150103a8ce430e85b5dd1e077f8f4ceafd
Dade1991 <dade91@msn.com>
2025-09-08 17:00:47 +0200

    Last commit

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -29,8 +29,21 @@ public class Exercise1 {
         System.out.println("_____ Exercise 1.3");
 
         String[] arrayOfStr = {"Primo", "Secondo", "Terzo", "Quarto", "Quinto"};
-        System.out.println(arrayOfStr);
 
+        System.out.println("Inserisci una stringa nuova al Terzo posto:");
+        String newStringThird = scannerTool.nextLine();
+
+        String[] newArrayOfStr = new String[6];
+
+        newArrayOfStr[0] = arrayOfStr[0];
+        newArrayOfStr[1] = arrayOfStr[1];
+        newArrayOfStr[2] = newStringThird;
+        newArrayOfStr[3] = arrayOfStr[2];
+        newArrayOfStr[4] = arrayOfStr[3];
+        newArrayOfStr[5] = arrayOfStr[4];
+        scannerTool.close();
+
+        System.out.println("Ecco il nuovo array di 6 elementi: " + newArrayOfStr);
 
         scannerTool.close();
     }
